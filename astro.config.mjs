12e20fcbde872e232c8ed2df3f05efbcfc16d64e
Kevin123Sacra123 <ksacramentojuarez@gmail.com>
2023-12-31 17:12:21 -0500

    subida 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
-
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/Kevin123Sacra123.github.io',
+  base: '/portafolio',
   integrations: [tailwind(), react(), alpinejs()]
 });
